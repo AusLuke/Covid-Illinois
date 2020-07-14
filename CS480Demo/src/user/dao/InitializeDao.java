e20ddf92dd6d10da7b50d2b6a908093b4e96d293
AusLuke <60331999+AusLuke@users.noreply.github.com>
2020-07-13 23:15:54 -0500

    Update to create covid tables

--- CS480Demo/src/user/dao/InitializeDao.java
+++ CS480Demo/src/user/dao/InitializeDao.java
@@ -32,7 +32,7 @@ public class InitializeDao {
 				              + "user=root&password=1234");
 			
 			statement = connect.createStatement();
-			statement.executeUpdate("DROP TABLE IF EXISTS tb_user");
+			statement.executeUpdate("DROP TABLE IF EXISTS tb_user;");
 			
 			String sqlstmt = "CREATE TABLE IF NOT EXISTS tb_user (" +
 							"username VARCHAR(50) NOT NULL PRIMARY KEY," + 
@@ -47,6 +47,67 @@ public class InitializeDao {
 					"('Luke2', 'Luke2', 'Luke2')," +
 					"('Luke3', 'Luke3', 'Luke3');";
 			
+			statement.executeUpdate(sqlstmt);
+			
+			
+			// create county_covid table and populate with 3 entries
+			statement.executeUpdate("DROP TABLE IF EXISTS county_covid");
+			
+			sqlstmt = "CREATE TABLE county_covid (\r\n" + 
+					"Date date NOT NULL,\r\n" + 
+					"County VARCHAR(100),\r\n" + 
+					"State VARCHAR(50),\r\n" + 
+					"FIPS INT NOT NULL,\r\n" + 
+					"Cases INT,\r\n" + 
+					"Deaths INT,\r\n" + 
+					"PRIMARY KEY(Date, FIPS)\r\n" + 
+					");";
+			statement.executeUpdate(sqlstmt);
+			
+			sqlstmt = "INSERT INTO county_covid VALUES\r\n" + 
+					"('2020-1-1', 'Autauga County', 'Alabama',  1, 10, 1),\r\n" + 
+					"('2020-1-1', 'Aleutians East Borough', 'Alaska',  2, 20, 2),\r\n" + 
+					"('2020-1-1', 'Apache County', 'Arizona',  4, 30, 3);";
+			statement.executeUpdate(sqlstmt);
+			
+			
+			// create state_covid table and populate with 3 entries
+			statement.executeUpdate("DROP TABLE IF EXISTS state_covid");
+			
+			sqlstmt = "CREATE TABLE state_covid (\r\n" + 
+					"Date date NOT NULL,\r\n" + 
+					"State VARCHAR(50),\r\n" + 
+					"FIPS INT NOT NULL,\r\n" + 
+					"Cases INT,\r\n" + 
+					"Deaths INT,\r\n" + 
+					"PRIMARY KEY(Date, FIPS)\r\n" + 
+					");";
+			statement.executeUpdate(sqlstmt);
+			
+			sqlstmt = "INSERT INTO state_covid VALUES\r\n" + 
+					"('2020-1-1', 'Alabama',  1, 10, 1),\r\n" + 
+					"('2020-1-1', 'Alaska',  2, 20, 2),\r\n" + 
+					"('2020-1-1', 'Arizona',  4, 30, 3);";
+			statement.executeUpdate(sqlstmt);
+			
+			
+			// create county_info table and populate with 3 entries
+			statement.executeUpdate("DROP TABLE IF EXISTS county_info");
+			
+			sqlstmt = "CREATE TABLE county_info (\r\n" + 
+					"FIPS INT NOT NULL,\r\n" + 
+					"CountyNum INT NOT NULL,\r\n" + 
+					"StateName VARCHAR(50),\r\n" + 
+					"CountyName VARCHAR(100),\r\n" + 
+					"Pop2019Est INT,\r\n" + 
+					"PRIMARY KEY(FIPS, CountyNum)\r\n" + 
+					");";
+			statement.executeUpdate(sqlstmt);
+			
+			sqlstmt = "INSERT INTO county_info VALUES\r\n" + 
+					"(1, 1, 'Alabama', 'Autauga County',55869),\r\n" + 
+					"(2, 13, 'Alaska', 'Aleutians East Borough', 3337),\r\n" + 
+					"(4, 1, 'Arizona', 'Apache County', 71887);";
 			statement.executeUpdate(sqlstmt);
 		
 		    
